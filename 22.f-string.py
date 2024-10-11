dd2a31c9c1355aad0da757a867b7745a5ba3e16c
Harshalkumar Kumre <40782195+HARSHALKUMRE@users.noreply.github.com>
2024-10-11 23:49:40 +0530

    add via file

---
+++ 22.f-string.py
@@ -0,0 +1,12 @@
+# s = "My name is {}. I am a {}"
+
+# name = "Harshal"
+# gender = "Male"
+
+# print(s.format(name, gender))
+
+name = "Harshal"
+gender = "Male"
+
+s = f"My name is {name}. I am a {gender}"
+print(s)
